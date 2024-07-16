53e78473dac3208e927729c6078a675d7a5e63b3
vitoralveschaves <vitorchavesalv@gmail.com>
2024-07-16 02:25:41 -0300

    feat: add MenuRepository interface

---
+++ src/main/java/com/example/menu/repository/MenuRepository.java
@@ -0,0 +1,11 @@
+package com.example.menu.repository;
+
+import com.example.menu.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, UUID> {
+}
